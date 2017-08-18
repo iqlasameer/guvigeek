437acd1076cdad1da25988abee4897ffdaff4ef0
iqlasameer <30856561+iqlasameer@users.noreply.github.com>
2017-08-18 20:18:02 +0530

    Update firstprog.c

--- firstprog.c
+++ firstprog.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void main()
+int main()
 {
 int a;
 printf("Enter the number:");
@@ -10,4 +10,5 @@ else if(a<0)
 printf("The entered number is negative");
 else if(a==0)
 printf("The entered number is zero");
+return 0;
 }
