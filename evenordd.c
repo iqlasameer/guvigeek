57c54d46139ac19ee24d2aaf7bd6e8ae30481f56
iqlasameer <30856561+iqlasameer@users.noreply.github.com>
2017-08-18 21:34:54 +0530

    eord

--- evenordd.c
+++ evenordd.c
@@ -1,25 +1,14 @@
 include<stdio.h>
-
 int main()
-
 {
 int a;
-
 printf("Enter the number:");
-
 scanf("%d",&a);
-
 if((a%2)==0)
-
 printf("The entered number is even");
-
 else if((a%2)!=0)
-
 printf("The entered number is odd");
-
 else
-
 printf("Invalid");
-
 return 0;
 }
