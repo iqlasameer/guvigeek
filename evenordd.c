68f45a32fcc139d53d26f4519655fc684bc557fa
iqlasameer <30856561+iqlasameer@users.noreply.github.com>
2017-08-18 20:55:50 +0530

    evenorodd

--- evenordd.c
+++ evenordd.c
@@ -6,7 +6,9 @@ printf("Enter the number:");
 scanf("%d",&a);
 if((a%2)==0)
 printf("The entered number is even");
-else
+else if((a%2)!=0)
 printf("The entered number is odd");
+else
+printf("Invalid");
 return 0;
 }
